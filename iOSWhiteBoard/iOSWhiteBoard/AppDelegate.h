5ffbf6cda611749b76179b4bd9eb436cb333cc22
bzhou <bzhou@grandstream.cn>
2017-03-24 12:01:24 +0800

    1)project init;

---
+++ iOSWhiteBoard/iOSWhiteBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSWhiteBoard
+//
+//  Created by ZhouDamon on 2017/3/24.
+//  Copyright © 2017年 Damon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
